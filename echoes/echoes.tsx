44aa305b784bf9f5058f4b3e93d115d47e426a7d
yhm ypa <103625580+yhm-amber@users.noreply.github.com>
2023-04-13 15:14:37 +0800

    Update echoes.tsx

--- echoes/echoes.tsx
+++ echoes/echoes.tsx
@@ -1,9 +1,19 @@
-export 
-const echoes = 
-(ffs: {[key: string]: (env: { [key: string]: Function }) => Function})
-: {[key: string]: Function} =>
-    Object.entries(ffs).reduce
-    (
-        (env, [fn, f]) => ({... env, [fn]: f(env)}) ,
-        {} as {[key: string]: Function}
-    ) ;
+namespace Echoes
+{
+    export 
+    const echoes =
+    <T = {[key: string]: any},> (waves: {[key: string]: (env: T) => any})
+    : T =>
+        Object.entries(waves).reduce
+        (
+            (envs, [fn, f]) => ({... envs, [fn]: f(envs)}) ,
+            {} as T
+        ) ;
+
+    export 
+    const call = 
+    <T extends Record<K, (...args: any) => any>, K extends keyof T>
+    (obj: T, key: K): { [P in K]: ReturnType<T[P]>; }[K] =>
+        echoes<{[P in K]: ReturnType<T[P]>}>(obj)[key] ;
+    
+} ;
