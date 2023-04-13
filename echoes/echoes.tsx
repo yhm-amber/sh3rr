18ba5bd31c5dd62bde3f3334ae455e963906257b
yhm ypa <103625580+yhm-amber@users.noreply.github.com>
2023-04-13 15:28:13 +0800

    Update echoes.tsx

--- echoes/echoes.tsx
+++ echoes/echoes.tsx
@@ -9,7 +9,7 @@ namespace Echoes
             (envs, [fn, f]) => ({... envs, [fn]: f(envs)}) ,
             {} as T
         ) ;
-
+    
     export 
     const call = 
     <T extends Record<K, (...args: any) => any>, K extends keyof T>
@@ -17,3 +17,40 @@ namespace Echoes
         echoes<{[P in K]: ReturnType<T[P]>}>(obj)[key] ;
     
 } ;
+
+// const ffs =
+// {
+//     f1: (env: { [key: string]: Function }) => 
+//         
+//         (n: number): number => 1 + n ,
+//     
+//     f2: (env: { [key: string]: Function }) => 
+//         
+//         (x: number): number => env.f1(x * 2) ,
+//     
+// } ;
+// 
+// console.log( Echoes.echoes(ffs).f2(3) ); // 7
+// 
+// const xx =
+// {
+//     x0: (env: { [key: string]: any }) => 
+//         
+//         1 ,
+//     
+//     f: (env: { [key: string]: any }) => 
+//         
+//         (s: string)
+//         : number => s.length ,
+//     
+//     f2: (env: { [key: string]: any }) => 
+//         
+//         (s: string, n: number)
+//         : Promise<number> => Promise.resolve(env.f(s) + n - env.x0) ,
+// } ;
+// 
+// Echoes.echoes<{f2: ReturnType<typeof xx.f2>}>(xx).f2('a',3).then(r => console.log(r)); // 3
+// Echoes.echoes(xx).f2('a',3).then( (r: number) => console.log(r) ); // 3
+// 
+// Echoes.call(xx,'f2')('a',3).then(r => console.log(r)); // 3
+// 
