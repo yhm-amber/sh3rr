84124b32479ce10403ce83be021db806800f3dee
yhm ypa <103625580+yhm-amber@users.noreply.github.com>
2023-04-27 11:49:52 +0800

    Create fp.sh

---
+++ fp/fp.sh/fp.sh
@@ -0,0 +1,26 @@
+#! /bin/env sh
+
+fp ()
+(
+    export NLINE=$'\n' &&
+    
+    # seq 2 2 8 | cat -n | f='echo "$x -> $y"' fp map x y
+    # echo a,b,c:d,e,f: | fielder=, f='echo "$z ~ $x -> $y"' fp map -d : -- y x z
+    map () (while IFS="${fielder:-$IFS}" read -r "${@:-p}" ;do eval "$f";done) &&
+    
+    # seq 7 | acc=3 f='echo $((x + acc))' fp reduce -- x
+    # echo a,b,c:d,e,f: | fielder=, acc='' f='echo "$y .. $z .. $x ~ $acc"' fp reduce -d : -- x y z
+    # echo a,b,c:d,e,f: | fielder=, acc='' f='echo "$acc: $y .. $z .. $x"' fp reduce -d : -- x y z
+    reduce () (while IFS="${fielder:-$IFS}" read -r "${@:-p}" ; do local acc="$(eval "$f")${concater:-$NLINE}" ; done ; echo "$(echo "$acc")") &&
+    reducer () (while IFS="${fielder:-$IFS}" read -r "${@:-p}" ; do local acc="${concater:-$NLINE}$(eval "$f")" ; done ; echo "$(echo "$acc")") &&
+    
+    # seq 2 2 8 | cat -n | f='"$x -> $y"' fp rdmap x y
+    # echo a,b,c:d,e,f: | fielder=, f='"$z ~ $x -> $y"' fp rdmap -d : -- y x z
+    rdmap () (acc='' f='printf '"'${formatter:-%s}'"' "$acc"'"$f" reduce "$@") &&
+    
+    "$@" &&
+    
+    : )
+
+: \
+fp "$@"
