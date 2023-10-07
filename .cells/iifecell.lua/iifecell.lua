bc0b9b6225a7e4203edb8985ca31d5dc8dfa1a42
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2023-10-07 15:57:01 +0800

    Update iifecell.lua

--- .cells/iifecell.lua/iifecell.lua
+++ .cells/iifecell.lua/iifecell.lua
@@ -1,13 +1,12 @@
 
-x = (
-function () 
+x = 
+( function () 
 
     a = 1 ;
     b = a + 1 ;
     c = function () return a + b end ;
 
-    return {a=a,c=c} end)
-() ;
+return {a=a,c=c} end ) () ;
 
 print (x.a) ; -- > 1
 print (x.c()) ; -- > 3
