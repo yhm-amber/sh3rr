a672457bd8e3bb46ef1b9d21e7ab7e7401a81ba8
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2023-10-07 16:02:26 +0800

    Update iifecell.lua

--- .cells/iifecell.lua/iifecell.lua
+++ .cells/iifecell.lua/iifecell.lua
@@ -11,3 +11,16 @@ return {a=a,c=c} end ) () ;
 print (x.a) ; -- > 1
 print (x.c()) ; -- > 3
 print (x.b) ; -- > nil
+
+--- 或者其实也可以这样
+--- 就是都要用完整名称会费劲一点
+
+xx = {}
+
+xx.a = 1 ;
+xx.b = xx.a + 1 ;
+xx.c = function () return xx.a + xx.b end ;
+
+print (xx.a) -- > 1
+print (xx.b) -- > 2
+print (xx.c()) -- > 3
