a43db3a83010f8064b60f6485dc4b099b8192748
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2026-06-12 17:12:51 +0800

    Update echoes.ts

--- .cells/echoes.ts/echoes.ts
+++ .cells/echoes.ts/echoes.ts
@@ -1,56 +1,63 @@
+
+// ── define ──────────────────────────────────────
 namespace Echoes
 {
-    export 
-    const echoes =
-    <T = {[key: string]: any},> (waves: {[key: string]: (env: T) => any})
-    : T =>
-        Object.entries(waves).reduce
-        (
-            (envs, [fn, f]) => ({... envs, [fn]: f(envs)}) ,
-            {} as T
-        ) ;
-    
-    export 
-    const call = 
-    <T extends Record<K, (...args: any) => any>, K extends keyof T>
-    (obj: T, key: K): { [P in K]: ReturnType<T[P]>; }[K] =>
-        echoes<{[P in K]: ReturnType<T[P]>}>(obj)[key] ;
-    
+	export 
+	const echoes = 
+	<T = {[key: string]: any},> (waves: {[key: string]: (env: T) => any})
+	: T => 
+		Object.entries(waves).reduce
+		(
+			(envs, [fn, f]) => ({... envs, [fn]: f(envs)}) ,
+			{} as T
+		) ;
+	
+	export 
+	const call = 
+	<T extends Record<K, (...args: any) => any>, K extends keyof T> 
+	(obj: T, key: K): { [P in K]: ReturnType<T[P]>; }[K] => 
+		echoes<{[P in K]: ReturnType<T[P]>}>(obj)[key] ;
+	
 } ;
 
-// const ffs =
-// {
-//     f1: (env: { [key: string]: Function }) => 
-//         
-//         (n: number): number => 1 + n ,
-//     
-//     f2: (env: { [key: string]: Function }) => 
-//         
-//         (x: number): number => env.f1(x * 2) ,
-//     
-// } ;
-// 
-// console.log( Echoes.echoes(ffs).f2(3) ); // 7
-// 
-// const xx =
-// {
-//     x0: (env: { [key: string]: any }) => 
-//         
-//         1 ,
-//     
-//     f: (env: { [key: string]: any }) => 
-//         
-//         (s: string)
-//         : number => s.length ,
-//     
-//     f2: (env: { [key: string]: any }) => 
-//         
-//         (s: string, n: number)
-//         : Promise<number> => Promise.resolve(env.f(s) + n - env.x0) ,
-// } ;
-// 
-// Echoes.echoes<{f2: ReturnType<typeof xx.f2>}>(xx).f2('a',3).then(r => console.log(r)); // 3
-// Echoes.echoes(xx).f2('a',3).then( (r: number) => console.log(r) ); // 3
-// 
-// Echoes.call(xx,'f2')('a',3).then(r => console.log(r)); // 3
-// 
+
+// ── demo ────────────────────────────────────────
+const env_ff = 
+{
+	f1: (env: { [key: string]: Function }) => 
+		
+		(n: number): number => 1 + n ,
+	
+	f2: (env: { [key: string]: Function }) => 
+		
+		(x: number): number => env.f1(x * 2) ,
+	
+} ;
+// ── effect ──────────────────────────────────────
+console.log( Echoes.echoes(env_ff).f2(3) ); //> 7
+
+
+
+// ── demo ────────────────────────────────────────
+const env_xx = 
+{
+	x0: (env: { [key: string]: any }) => 
+		
+		1 ,
+	
+	f: (env: { [key: string]: any }) => 
+		
+		(s: string)
+		: number => s.length ,
+	
+	f2: (env: { [key: string]: any }) => 
+		
+		(s: string, n: number)
+		: Promise<number> => Promise.resolve(env.f(s) + n - env.x0) ,
+} ;
+
+// ── effect ──────────────────────────────────────
+Echoes.echoes<{f2: ReturnType<typeof env_xx.f2>}>(env_xx).f2('a',3).then(r => console.log(r)); //> 3
+Echoes.echoes(env_xx).f2('a',3).then( (r: number) => console.log(r) ); //> 3
+Echoes.call(env_xx,'f2')('a',3).then(r => console.log(r)); //> 3
+
